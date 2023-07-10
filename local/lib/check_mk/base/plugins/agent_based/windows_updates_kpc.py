34ff10d380fd76de1b7bd89d22b4cc881185a78d
matthias1232 <matthias1232@users.noreply.github.com>
2023-07-10 15:48:04 +0200

    Update windows_updates_kpc.py

--- local/lib/check_mk/base/plugins/agent_based/windows_updates_kpc.py
+++ local/lib/check_mk/base/plugins/agent_based/windows_updates_kpc.py
@@ -44,9 +44,9 @@ def check_windows_updates_kpc(item, params, section):
 
     mandatorywarn = params["warning_lower"][0]
     mandatorycrit = params["warning_lower"][1]
-    criticalywarn = params["warning_lower"][2]
-    criticalycrit = params["warning_lower"][3]
-    importantywarn = params["warning_lower"][4]
+    criticalwarn = params["warning_lower"][2]
+    criticalcrit = params["warning_lower"][3]
+    importantwarn = params["warning_lower"][4]
     importantcrit = params["warning_lower"][5]
     moderatewarn = params["warning_lower"][6]
     moderatecrit = params["warning_lower"][7]
@@ -106,13 +106,73 @@ def check_windows_updates_kpc(item, params, section):
         support = "\n \n \n For Support and Sales Please Contact K&P Computer! \n \n E-Mail: hds@kpc.de \n \n 24/7 Helpdesk-Support: \n International: +800 4479 3300 \n Germany: +49 6122 7071 330 \n Austria: +43 1 525 1833 \n\n Web Germany: https://www.kpc.de \n Web Austria: https://www.kpc.at \n Web International: https://www.kpc.de/en"
 
 
-
         if jobname_windows_updates_kpc != item:
             continue  # Skip not matching lines
 
+        state = ""
+        statemandatory = " (OK)"
+        statecritical = " (OK)"
+        stateimportant = " (OK)"
+        statemoderate = " (OK)"
+        statelow = " (OK)"
+        stateunspecified = " (OK)"
 
-        state = State.OK
-        summarytext= "Mandatory: " + Mandatorycount + ", Critical: " + Criticalcount + ", Important: " + Importantcount + ", Moderate: " + Moderatecount + ", Low: " + Lowcount + ", Unspecified: " + Unspecifiedcount
+
+        if int(Mandatorycount) >= int(mandatorywarn):
+             statemandatory = " (WARN)"
+        if int(Mandatorycount) >= int(mandatorycrit):
+             statemandatory = " (CRIT)"
+        if int(Criticalcount) >= int(criticalwarn):
+             statecritical = " (WARN)"
+        if int(Criticalcount) >= int(criticalcrit):
+             statecritical = " (CRIT)"
+        if int(Importantcount) >= int(importantwarn):
+             stateimportant = " (WARN)"
+        if int(Importantcount) >= int(importantcrit):
+             stateimportant = " (CRIT)"
+        if int(Moderatecount) >= int(moderatewarn):
+             statemoderate = " (WARN)"
+        if int(Moderatecount) >= int(moderatecrit):
+             statemoderate = " (CRIT)"
+        if int(Lowcount) >= int(lowwarn):
+             statelow = " (WARN)"
+        if int(Lowcount) >= lowcrit:
+             statelow = " (CRIT)"
+        if int(Unspecifiedcount) >= int(unspecifiedwarn):
+             stateunspecified = " (WARN)"
+        if int(Unspecifiedcount) >= int(unspecifiedcrit):
+             stateunspecified = " (CRIT)"
+
+
+        if int(Mandatorycount) >= int(mandatorywarn) and state != State.CRIT:
+             state = State.WARN
+        if int(Mandatorycount) >= int(mandatorycrit):
+             state = State.CRIT
+        if int(Criticalcount) >= int(criticalwarn) and state != State.CRIT:
+             state = State.WARN
+        if int(Criticalcount) >= int(criticalcrit):
+             state = State.CRIT
+        if int(Importantcount) >= int(importantwarn) and state != State.CRIT:
+             state = State.WARN
+        if int(Importantcount) >= int(importantcrit):
+             state = State.CRIT
+        if int(Moderatecount) >= int(moderatewarn) and state != State.CRIT:
+             state = State.WARN
+        if int(Moderatecount) >= int(moderatecrit):
+             state = State.CRIT
+        if int(Lowcount) >= int(lowwarn) and state != State.CRIT:
+             state = State.WARN
+        if int(Lowcount) >= lowcrit:
+             state = State.CRIT
+        if int(Unspecifiedcount) >= int(unspecifiedwarn) and state != State.CRIT:
+             state = State.WARN
+        if int(Unspecifiedcount) >= int(unspecifiedcrit):
+             state = State.CRIT
+        if state != State.WARN and state != State.CRIT:
+             state = State.OK
+
+
+        summarytext= "Mandatory: " + Mandatorycount + statemandatory + ", Critical: " + Criticalcount + statecritical + ", Important: " + Importantcount + stateimportant + ", Moderate: " + Moderatecount + statemoderate + ", Low: " + Lowcount + statelow + ", Unspecified: " + Unspecifiedcount + stateunspecified 
         summarydetails = updatelist + Mandatoryupdates + Criticalupdates + Importantupdates + Moderateupdates + Lowupdates + Unspecifiedupdates + support
 
         yield Result(
